b58dc46b1f6014391a8fc5257382cc9b24310bcf
mrhaffner <mrhaffner13@gmail.com>
2021-11-04 18:19:02 -0500

    refactor: changed the organized of output snow_data

--- main.py
+++ main.py
@@ -6,16 +6,10 @@ from datetime import datetime
 from dotenv import dotenv_values
 config = dotenv_values(".env")
 
-#once daily (can serve to wake up java api as well)
-#loop over list of resorts
-    #get snow, and date reported
-    #add slug, snow, date reported and current date (date collected) to object
-        #jsonify object, send to java api
-
 f = open('resorts.json')
 data = json.load(f)
 
-snow_data = {}
+snow_data = {'data': []}
 
 for key, val in data.items():
     page = requests.get(val)
@@ -23,18 +17,26 @@ for key, val in data.items():
     yesterday = soup.find_all('div', string=config['STRING'])
     parent = yesterday[0].parent
 
-    snow = parent.find('div', config['SNOW']).text.strip().replace('\"', '')
+    snow = parent.find('div', config['SNOW']).text.strip().replace('"', '')
 
     date = parent.find('div', config['DATE']).text.strip()
     converted_date = str(datetime.strptime(date + ' 2021', '%b %d %Y')).split(" ")[0]
 
     current_date = datetime.today().strftime('%Y-%m-%d')
-    snow_data[key] = {
+    snow_data['data'].append({
+        'name': key,
         'snow': snow,
-        'date_reported': converted_date,
+        'date_snowfall': converted_date,
         'date_collected': current_date
-    }
-    time.sleep(1) 
+    })
+    time.sleep(1)
+
 print(snow_data)
+
+#schedule this to run once per day
+#probably want a header with security code
+# r = requests.post(config['POST_URI'], data=json.dumps(snow_data))
+# print(r.status_code, r.reason)
+# print(r.text[:300] + '...')
 
 f.close()
